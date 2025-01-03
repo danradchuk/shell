57152051f628e773bf9e70a3d73fc81af5e0f3d7
danradchuk <radchuk@fastmail.com>
2025-01-03 22:54:18 +0300

    background jobs

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/_types/_pid_t.h>
 #include <sys/signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -13,6 +12,10 @@ volatile sig_atomic_t pid;
 volatile sig_atomic_t fg_pid;
 
 static void handle_sigint(int sig) {
+  // char buf[256];
+  // int len = snprintf(buf, sizeof(buf), "\n[%d]\n", fg_pid);
+  // write(STDOUT_FILENO, buf, len);
+
   if (fg_pid > 0) { // there is a foreground task
     // Kill a child's process group if it's running
     kill(-fg_pid, SIGINT);
@@ -21,17 +24,41 @@ static void handle_sigint(int sig) {
   }
 }
 
-static void handle_sigchld(int sig) { pid = waitpid(-1, NULL, 0); }
+static void handle_sigchld(int sig) {
+  // pid = waitpid(-1, NULL, 0); // must be while loop to reap all children
+  // // printf("%d\n", pid);
+  // if (pid != fg_pid) {
+  //   char buf[256];
+  //   int len = snprintf(buf, sizeof(buf), "\n[%d] has ended.\n", pid);
+  //   write(STDOUT_FILENO, buf, len);
+  //   write(STDOUT_FILENO, "sh:$ ", 5);
+  // }
+
+  int status;
+  pid_t waited_pid;
+
+  while ((waited_pid = waitpid(-1, &status, WNOHANG)) > 0) {
+    if (waited_pid != fg_pid) {
+      char buf[256];
+      int len = snprintf(buf, sizeof(buf), "\n[%d] has ended.\n", waited_pid);
+      write(STDOUT_FILENO, buf, len);
+      write(STDOUT_FILENO, "sh:$ ", 5);
+    }
+    pid = waited_pid;
+  }
+}
 
 int main(int argc, char **argv) {
+  int bg;
   sigset_t new_mask, old_mask;
 
-  // handle sigint
+  // handle SIGINT
   if (signal(SIGINT, handle_sigint) == SIG_ERR) {
     perror("signal");
     exit(1);
   }
 
+  // handle SIGCHLD
   if (signal(SIGCHLD, handle_sigchld) == SIG_ERR) {
     perror("signal");
     exit(1);
@@ -42,6 +69,7 @@ int main(int argc, char **argv) {
 
   for (;;) {
     printf("sh:$ ");
+
     char *input = NULL;
     size_t len = 0;
     ssize_t read;
@@ -59,7 +87,16 @@ int main(int argc, char **argv) {
     tokenize_user_input(&slice, input);
     // print_slice(&slice);
     char *cmd = get_slice_elem(&slice, 0);
-    append_null(&slice); // required for calling execvp
+    char *amp = get_slice_elem(&slice, (&slice)->len - 1);
+
+    // handle background job sign
+    if (strcmp(amp, "&") == 0) {
+      bg = 1;
+      (&slice)->data[(&slice)->len - 1] = NULL;
+    } else {
+      bg = 0;
+      append_null(&slice); // required for calling execvp
+    }
 
     // handle exit
     if (strcmp(cmd, "exit") == 0) {
@@ -67,7 +104,9 @@ int main(int argc, char **argv) {
     }
 
     // fork & exec
-    sigprocmask(SIG_BLOCK, &new_mask, &old_mask);
+    if (!bg) {
+      sigprocmask(SIG_BLOCK, &new_mask, &old_mask);
+    }
 
     pid_t c_pid = fork();
     if (c_pid < 0) {
@@ -76,20 +115,28 @@ int main(int argc, char **argv) {
     }
 
     if (c_pid == 0) {
-      setpgid(0, 0);
+      setpgid(0, 0); // create a new process group with PGID = c_pid
       signal(SIGINT, SIG_DFL);
 
       if (execvp(cmd, (&slice)->data) == -1) {
+        free(input);
+        dispose_slice(&slice);
+
         perror("execvp failed");
         exit(1);
       }
     } else {
-      fg_pid = c_pid;
-      pid = 0;
-      while (!pid) {
-        sigsuspend(&old_mask);
+      if (!bg) {
+        fg_pid = c_pid;
+        printf("Parent: %d\n", fg_pid);
+        pid = 0;
+        while (!pid) {
+          sigsuspend(&old_mask);
+        }
+        sigprocmask(SIG_SETMASK, &old_mask, NULL);
+      } else {
+        printf("%d %s has started\n", c_pid, cmd);
       }
-      sigprocmask(SIG_SETMASK, &old_mask, NULL);
     }
 
     free(input);
