3be52e4f3f7a3fdb261bf229ee2d9c3839a4624f
danradchuk <radchuk@fastmail.com>
2025-01-06 19:51:19 +0300

    refactor slice

--- include/slice.h
+++ include/slice.h
@@ -1,19 +1,19 @@
-#ifndef SLICE_H 
-#define SLICE_H 
+#ifndef SLICE_H
+#define SLICE_H
 
 #include <stdint.h>
 
 typedef struct {
-    char** data;
-    uint32_t len;
-    uint32_t cap;
+  char **data;
+  uint32_t len;
+  uint32_t cap;
 } Slice;
 
-void init_slice(Slice* slice, uint32_t cap);
-void append_slice(Slice* slice, const char* elem);
-void append_null(Slice* slice);
-char* get_slice_elem(Slice* slice, uint32_t idx);
-void dispose_slice(Slice* slice);
-void print_slice(Slice* slice);
+void init_slice(Slice *slice, uint32_t cap);
+void append_slice(Slice *slice, const char *elem);
+void append_null(Slice *slice);
+char *get_slice_elem(Slice *slice, uint32_t idx);
+void dispose_slice(Slice *slice);
+void print_slice(Slice *slice);
 
-#endif  // SLICE_H
+#endif // SLICE_H
