b4de08fe07261332aabdfab0325a067f88c8c0c8
danradchuk <radchuk@fastmail.com>
2025-01-10 22:53:32 +0300

    refactor utils

--- include/utils.h
+++ include/utils.h
@@ -3,13 +3,6 @@
 
 #include "slice.h"
 
-int extract_cmd_size(char *input);
-char *extract_cmd(char *input, int size);
-
-int extract_arguments_size(char *input, int offset);
-char *extract_arguments(char *input, int size);
-
 void tokenize_user_input(Slice *s, char *input);
-
 void remove_trailing_whitespaces(char *str);
 #endif
